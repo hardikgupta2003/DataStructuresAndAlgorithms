6392b1472a2291b0c72b64765d1ed84c5610c35e
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-09-06 23:59:13 +0530

    added patterns

---
+++ Patterns/HollowInvertedHalfPyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the size of pyramid : ";
+    cin>>n;
+   
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+           if(j==0 || j==n-i)
+                cout<<"*";
+            
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
