738abafaff17bcc421de62230bc0ca7478f80625
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-09-07 01:17:05 +0530

    Added Patterns

--- Patterns/HalfPyramidNum2.cpp
+++ Patterns/HalfPyramidNum2.cpp
@@ -7,23 +7,21 @@ int main(){
     cin>>n;
    
     for (int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            if(i==0 || i==n-1){
-               cout<<"* ";
+        for(int j=0;j<i;j++){
+           if(i==0 || i==n-1){
+               cout<<j+1<<" ";
            
             }
            else{
-             if(j==0 || j==n-i-1 ){
-            cout<<"* ";
+
+            if(j==0 || j==n-i ){
+            cout<<j+1<<" ";
            }
            else{
              cout<<"  ";
            }
             
            }
-                
-            
-            
         }
         cout<<endl;
     }
