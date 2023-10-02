363febe38e3574a86369eee6b39b1473fe591e14
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-10-02 22:16:58 +0530

    PivotIndex added

---
+++ Searching And Sorting/FindPivotIndex.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fintPivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int s=0;
+        int e=n-1;
+        int mid=(s+e)/2;
+
+        while(s<=e){
+            if(nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid-1]){
+                
+                return mid-1;
+
+            }
+            else if(nums[s]>nums[mid]){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            //mid update
+            mid=(s+e)/2;
+        }
+        return -1;
+    }
+
+int main(){
+
+    vector<int> arr{10,12,14,16,2,4,6,8};
+    
+    int index = fintPivotIndex(arr);
+    cout<<"pivot index  : "<<index<<endl;
+    return 0;
+}
