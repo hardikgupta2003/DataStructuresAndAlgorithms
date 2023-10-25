bbd72876fe45c1713176badae4412092a8e52237
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-10-25 11:47:32 +0530

    added optimized

--- Arrays and Vectors/FindDuplicate.cpp
+++ Arrays and Vectors/FindDuplicate.cpp
@@ -4,16 +4,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
-        }
-        return -1;
-    }
+// int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]==nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+  int findDuplicate(vector<int>& nums) {
+       int ans=-1;
+       for(int i=0;i<nums.size();i++){
+           int index=abs(nums[i]);
 
+           //already visited
+           if(nums[index]<0){
+               ans=index;
+               break;
+           }
+
+           //not visited? then mark visited
+           nums[index]*=-1;
+
+       }
+       return ans;
+    }
 int main(){
     vector<int> nums{1,3,4,2,2};
     int ans=findDuplicate(nums);
