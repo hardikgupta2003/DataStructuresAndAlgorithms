6e04b3d39bbf357cc400edbb5e6c3994958ea4d5
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-12-29 10:42:32 +0530

    added oops

---
+++ OOPS/ConstKeyword.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // const int x=10;
+    // x=20;
+    // cout<<x<<endl;
+
+    // const with pointers 
+    // 1}
+//    const int *a= new int (2);//const data,non const pointer
+// //    *a=20;
+//     cout<<*a<<endl;
+//     int b=40;
+//     a=&b;
+//     cout<<*a<<endl;
+
+// 2}
+// Const pointer , non const data 
+// int *const a=new int (2);
+// cout<<*a<<endl;
+// *a=60;
+// cout<<*a<<endl;
+// int b=50;
+// a=&b;
+// cout<<*a<<endl;
+
+// 3}
+// const pointer ,const data 
+// const int *const a=new int(59);
+// cout<<*a<<endl;
+// *a=50;
+// cout<<*a<<endl;
+// int b=100;
+// a=&b;
+// cout<<*a<<endl;
+
+
+
+
+    return 0;
+
+}
