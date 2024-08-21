cc0c99eaee0c7c1302e9b03ce0fa19d91403a17f
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-08-21 16:20:04 +0530

    Added Binary Search Question

---
+++ Searching And Sorting/H-Index.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int start = 0;
+        int end = n - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (citations[mid] == n - mid)
+            {
+                return n - mid;
+            }
+            else if (citations[mid] > n - mid)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return n - start;
+    }
+};
+
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int index = 0;
+        while (index < n && n - index > citations[index])
+        {
+            index++;
+        }
+        return n - index;
+    }
+};
