91009cf8b82df56209aeb6ef783c57114d4d72df
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-09-02 23:34:26 +0530

    Added Array Leetcode Qs

---
+++ Arrays and Vectors/FindStudentsWillReplaceChalk.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int chalkReplacer(vector<int> &chalk, int k)
+    {
+        int n = chalk.size();
+        long long sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += chalk[i];
+        }
+        k = k % sum;
+        for (int i = 0; i < n; i++)
+        {
+            if (chalk[i] > k)
+            {
+                return i;
+            }
+            k -= chalk[i];
+        }
+        return -1;
+    }
+};
