5330f12a710dbb8d97f405cd5d4b5cbbcde6b90b
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-09-08 23:23:48 +0530

    Added Leetcode LL Qs

---
+++ LinkedList/SplitLLintoParts.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    vector<ListNode *> splitListToParts(ListNode *head, int k)
+    {
+        // 1 - LL ki length nikal lo
+        ListNode *curr = head;
+        int L = 0;
+        while (curr)
+        {
+            L++;
+            curr = curr->next;
+        }
+
+        int eachBucketNodes = L / k;
+        int remainderNodes = L % k;
+
+        vector<ListNode *> result(k, NULL);
+
+        curr = head;
+        ListNode *prev = NULL;
+        for (int i = 0; i < k; i++)
+        {
+            {
+                result[i] = curr;
+                for (int count = 1;
+                     count <= eachBucketNodes + (remainderNodes > 0 ? 1 : 0);
+                     count++)
+                {
+                    prev = curr;
+                    curr = curr->next;
+                }
+                if (prev != NULL)
+                {
+                    prev->next = NULL;
+                }
+                remainderNodes--;
+            }
+        }
+        return result;
+    }
+};
