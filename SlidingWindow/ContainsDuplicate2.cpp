f007448bb917577aedb3c6fa1405dd17345f50a9
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-09-18 16:54:45 +0530

    Added Sliding Window LC QS

--- SlidingWindow/ContainsDuplicate2.cpp
+++ SlidingWindow/ContainsDuplicate2.cpp
@@ -1,6 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
+// sliding window
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        int n = nums.size(); // Size of the input array
+
+        unordered_set<int> st; // Set to store unique elements within the current window
+        int i = 0, j = 0;      // Two pointers: i (left) and j (right) for the sliding window
+
+        // Sliding window approach
+        while (j < n)
+        {
+            // Step 1: Ensure the window size does not exceed k
+            if (abs(i - j) > k)
+            {
+                st.erase(nums[i]); // Remove the element at the left of the window
+                i++;               // Shrink the window from the left
+            }
+
+            // Check if the current element nums[j] is already in the set (meaning it appeared within the window)
+            if (st.find(nums[j]) != st.end())
+            {
+                return true; // If found, return true (duplicate found within distance k)
+            }
+
+            // Otherwise, add the current element nums[j] to the set and expand the window
+            st.insert(nums[j]);
+            j++; // Move the right pointer to expand the window
+        }
+
+        // If no duplicates are found within distance k, return false
+        return false;
+    }
+};
+
 class Solution
 {
 public:
@@ -25,3 +64,5 @@ public:
         return false;
     }
 };
+
+
--- SlidingWindow/MinimumWindowSubstring.cpp
+++ SlidingWindow/MinimumWindowSubstring.cpp
@@ -6,58 +6,66 @@ class Solution
 public:
     string minWindow(string s, string t)
     {
-        int n = s.size();
+        int n = s.length(); // Length of string 's'
 
-        if (t.size() > n)
+        // If 't' is longer than 's', it's impossible to have a valid substring
+        if (t.length() > n)
+        {
             return "";
+        }
 
         unordered_map<char, int> mp;
-
-        // store karliya
-        for (char &ch : t)
+        // Populate frequency map for characters in 't'
+        for (auto &ch : t)
         {
             mp[ch]++;
         }
 
-        int requiredCount = t.size();
-        int i = 0, j = 0;
+        int requiredCount = t.length(); // Total characters we need to match
+        int i = 0, j = 0;               // 'i' and 'j' are the two pointers for the sliding window
 
-        int minWindowSize = INT_MAX;
-        int start_i = 0;
+        int minWindowSize = INT_MAX; // To store the minimum window size
+        int start_i = 0;             // To store the starting index of the smallest window
 
-        // story starts
+        // Start expanding the window by moving 'j'
         while (j < n)
         {
-            char ch = s[j];
+            char ch = s[j]; // Get the current character in the window
 
+            // If this character is in 't', reduce the required count
             if (mp[ch] > 0)
-            { // map me present hai
+            {
                 requiredCount--;
             }
-
+            // Decrease the frequency of the current character in the map
             mp[ch]--;
 
+            // If all characters are matched (requiredCount == 0), start shrinking the window
             while (requiredCount == 0)
             {
-                // start shrinking the window
+                int currWindowSize = j - i + 1; // Current window size
 
-                int currWindowSize = j - i + 1;
-
+                // Update the minimum window size if we find a smaller valid window
                 if (minWindowSize > currWindowSize)
                 {
                     minWindowSize = currWindowSize;
-                    start_i = i;
+                    start_i = i; // Update the start index of the minimum window
                 }
-                mp[s[i]]++;
 
+                // Move the left pointer 'i' to shrink the window
+                mp[s[i]]++; // Increment the frequency of the character at 'i'
+
+                // If this character becomes part of 't' again, we need to match it, so increase the requiredCount
                 if (mp[s[i]] > 0)
                 {
                     requiredCount++;
                 }
-                i++;
+                i++; // Move the left pointer to shrink the window
             }
-            j++;
+            j++; // Expand the window by moving the right pointer 'j'
         }
+
+        // If no valid window was found, return an empty string
         return minWindowSize == INT_MAX ? "" : s.substr(start_i, minWindowSize);
     }
 };
